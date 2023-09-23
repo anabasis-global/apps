518ce2980ea025c907aa4aa858419aa6f15893df
Anabasis <136603493+anabasis-global@users.noreply.github.com>
2023-09-24 01:51:16 +0300

    Update glpi.ps1

--- scripts/powershell/glpi.ps1
+++ scripts/powershell/glpi.ps1
@@ -1,10 +1,12 @@
 
-  $options = @{
-  ADD_FIREWALL_EXCEPTION=1
-  ADDLOCAL="ALL"
-  EXECMODE=1
-  RUNNOW=1
-  SCAN_HOMEDIRS=1
-  SERVER="https://its.anabasis.pro/glpi/inventory"
+  $config = @{
+    TAG="office"
+    ADD_FIREWALL_EXCEPTION=1
+    ADDLOCAL="ALL"
+    EXECMODE=1
+    RUNNOW=1
+    SCAN_HOMEDIRS=1
+    SERVER=""
   }
+  
   $SetupOptions = "/quiet ADD_FIREWALL_EXCEPTION=1 ADDLOCAL=ALL ADDLOCAL=60 EXECMODE=1 RUNNOW=1 SCAN_HOMEDIRS=1 SERVER=$SetupServer"
